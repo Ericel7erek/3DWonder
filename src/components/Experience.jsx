94b14fcd5d71f899056360bc8cfadf130bba0314
ericel7erek <ericalaa@ymail.com>
2024-05-13 18:58:33 +0200

    Dragoon

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 import * as THREE from "three"
+import { Dragon } from "./Dragon";
 export const Experience = () => {
   const map = useTexture("textures/f9cd8fac-841f-4c02-bd17-6f5e5f1b7d86.jpeg")
   return (
@@ -7,6 +8,7 @@ export const Experience = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
+      <Dragon scale={0.6} position-y={-1}/>
       <mesh>
         <sphereGeometry args={[5,64,64]} />
         <meshBasicMaterial map={map} side={THREE.BackSide}/>
